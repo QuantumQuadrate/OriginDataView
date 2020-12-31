bb4eff71212b9c467de8fcdf5d6fb93d31f19c30
bpnordin <bpnordin@gmail.com>
2020-12-31 14:11:36 -0600

    first commit

---
+++ GetData.py
@@ -0,0 +1,5 @@
+import zmq
+
+
+def get_data(read_sock, stream,config, timeout = 300):
+    pass
--- first_app.py
+++ first_app.py
@@ -7,41 +7,126 @@ import streamlit as st
 import numpy as np
 import time
 import pandas as pd
+from loguru import logger
 
-st.write("Here's our first attempt at using data to create a table:")
-st.write(pd.DataFrame({
-    'first column': [1, 2, 3, 4],
-    'second column': [10, 20, 30, 40]
-}))
+import configparser
+import zmq
+import json
+import GetData as reciever
+from streamlit.report_thread import get_report_ctx
 
-if st.checkbox('Show dataframe'):
-    chart_data = pd.DataFrame(
-       np.random.randn(20, 3),
-       columns=['a', 'b', 'c'])
+ctx = get_report_ctx()
+session_id = ctx.session_id
 
-    st.line_chart(chart_data)
+@st.cache(hash_funcs={zmq.sugar.socket.Socket: id})
+def create_socket_sub(session_id):
+    config = configparser.ConfigParser(inline_comment_prefixes = ';')
+    config.read("origin-server.cfg")
+    ip = config.get('Server','ip')
+    sub_port = config.getint('Server', 'pub_port')
 
-'Starting a long computation...'
+    context = zmq.Context()
+    sub_sock = context.socket(zmq.SUB)
+    sub_sock.connect("tcp://{}:{}".format(ip,sub_port))
+    logger.debug("Connected to sub socket id: {}".format(id(sub_sock)))
+    return sub_sock
+    
+@st.cache(hash_funcs={zmq.sugar.socket.Socket: id})
+def create_socket_read():
+    config = configparser.ConfigParser(inline_comment_prefixes = ';')
+    config.read("origin-server.cfg")
+    ip = config.get('Server','ip')
+    read_port = config.getint('Server','read_port')
 
-# Add a placeholder
-latest_iteration = st.empty()
-bar = st.progress(0)
+    context = zmq.Context()
+    read_sock = context.socket(zmq.REQ)
+    read_sock.connect("tcp://{}:{}".format(ip,read_port))
+    logger.debug("Connected to read socket")
+    return read_sock
 
-for i in range(100):
-  # Update the progress bar with each iteration.
-  latest_iteration.text(f'Iteration {i+1}')
-  bar.progress(i + 1)
-  time.sleep(0.1)
+@st.cache(hash_funcs={zmq.sugar.socket.Socket: id})
+def get_available_streams(read_sock):
+    """!@brief Request the knownStreams object from the server.
+    @return knownStreams
+    """
+    # Sending an empty JSON object requests an object containing the
+    # available streams
+    logger.debug("read sock id: {}".format(id(read_sock)))
+    
+    read_sock.send_string('{}')
+    try:
+        err, known_streams = json.loads(read_sock.recv())
+    except:
+        logger.error("Error connecting to data server")
+        st.write("Error connecting to data server")
+    return known_streams
 
-'...and now we\'re done!'
+#set up the site
+#get all of the streams we can subscribe to
+read_sock = create_socket_read()
+stream_dict = get_available_streams(read_sock)["streams"]
+known_streams = list(stream_dict.keys())
+check_boxes = [st.sidebar.checkbox(stream, key=stream) for stream in known_streams]
+checked_streams = [stream for stream,checked in zip(known_streams,check_boxes) if checked]
 
-df = pd.DataFrame({
-  'first column': [1, 2, 3, 4],
-  'second column': [10, 20, 30, 40]
-})
+sub_boolean = False
 
-option = st.selectbox(
-    'Which number do you like best?',
-     df['first column'])
+@st.cache(ttl=60*10)
+def get_stream_filter(stream_dict,stream,length = 4):
+    stream_id = str(stream_dict[stream]["id"]).zfill(length)
+    return stream_id
+#here is where we will start the sub port and read
 
-'You selected: ', option
+start_button = st.button("Start Subscribing")
+graph = st.empty()
+stop_button = st.button("Stop Subscribing")
+    
+if start_button:
+    try:
+        sub_sock.close()
+    except:
+        pass
+    sub_sock = create_socket_sub(session_id)
+    for stream in checked_streams:
+        sub_sock.setsockopt_string(zmq.SUBSCRIBE, get_stream_filter(stream_dict,stream))
+    st.write(checked_streams)
+    sub_boolean = True
+    st.write("Started Subscribing")
+
+#loop to get the data from subscriber and graph it 
+count = 0
+DATA = {}
+while sub_boolean:
+    try:
+        [streamID, content] = sub_sock.recv_multipart()
+        content =json.loads(content.decode('ascii','strict'))
+        dat = {key:content[key] for key in content if key != "measurement_time"}
+        content = pd.DataFrame(dat, index=[content["measurement_time"]])
+
+        if streamID not in DATA:
+            #add the data
+            DATA[streamID] = content
+        else:
+            #append the data
+            DATA[streamID] = DATA[streamID].append(content)
+            if DATA[streamID].size > 1000:
+                #remove the first element
+                DATA[streamID].drop(DATA[streamID].head(1).index,inplace=True)
+        count = count + 1
+        for key in DATA:
+            graph.line_chart(DATA[key])
+        time.sleep(.01)
+    except zmq.ZMQError as e:
+        logger.debug(e)
+        #all of the proper shutdown calls
+        sub_sock.close()
+        st.write("Connection error, closing subscription")
+        logger.error("Connection error, closing subscription")
+        sub_boolean = False
+    
+    if stop_button:
+        sub_sock.close()
+        st.write("Breaking while loop")
+        sub_boolean = False
+    
+
