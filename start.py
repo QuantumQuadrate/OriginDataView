1821bbf7448c57f55e5f4d6f344e51915a6dccc3
bpnordin <bpnordin@gmail.com>
2021-01-05 16:47:49 -0600

    updated readme

--- start.py
+++ start.py
@@ -25,7 +25,10 @@ st.set_page_config(layout='wide')
 
 stream_selection = st.sidebar
 readme_expander = st.beta_expander(label="readme")
-readme_expander.write("""Every interaction with a widget will rerun the whole script and 
+readme_expander.write("""
+To start, select the Origin streams from the sidebar. This sidebar can be collapsed
+
+Every interaction with a widget, but not the dropdowns, will rerun the whole script and 
 stop the subscription execution.
 Just click the start subscribing button to resubscribe.
 
